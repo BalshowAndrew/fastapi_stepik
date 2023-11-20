fd00005dad52a5af787a0faf4f15649e199a1dae
Andrew <balshowandrew@gmail.com>
2023-11-20 20:52:59 +0300

    maked directs models and roters

--- fastapi_stepik/main.py
+++ fastapi_stepik/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class User(BaseModel):
+    username: str
+    age: int
+    message: str
+    
 
+@app.post("/")
+async def root(user: User):
+    print(f"Мы получили от юзера {user.username}, которому {user.age} такое сообщение: {user.message}")
+    return user
 
 @app.get("/index", response_class=FileResponse)
 async def root_index():
@@ -18,6 +25,11 @@ async def root_index():
 @app.post("/calculate")
 async def root_calculate(num1: int, num2: int):
     return {"result": f"{num1 + num2}"}
+
+
+@app.get("/custom")
+async def read_custom_message():
+    return {"message": "This is a custom message!"}
 
 
 
